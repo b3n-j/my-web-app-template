abefdd6cba5eb4616897b472f10f2da7b34bb548
b3n-j <b3ngrc@gmail.com>
2025-01-30 17:39:35 +0100

    Created app template with custom architecture using Next.js, React, Tailwind, Shadcn/ui, Supabase

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Header from '@/app/_components/layout/header';
+import ConnectSupabaseSteps from '@/app/_components/widgets/connect-supabase-steps';
+import SignUpUserSteps from '@/app/_components/widgets/sign-up-user-steps';
+import { hasEnvVars } from '@/app/_utils/supabase/check-env-vars';
+
+export default async function Home() {
+  return (
+    <>
+      <Header />
+      <main className="flex-1 flex flex-col gap-6 px-4">
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>
+        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      </main>
+    </>
+  );
+}
